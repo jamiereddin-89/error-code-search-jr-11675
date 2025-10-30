4f628631c3aaddd0e4490765d9cc0f51c139fd56
jamiereddin-89 <jamiereactive23@gmail.com>
2025-10-30 03:39:49 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/', // Use root path for Lovable deployment
+  base: '/error-code-search-jr-11675/', // Use root path for Lovable deployment
   server: {
     host: "0.0.0.0",
     port: 8080,
